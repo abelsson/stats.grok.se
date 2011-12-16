e8dfe3b1daefba2838919169ebdcde3000b8d95c
Henrik Abelsson <henrik@bulganin.(none)>
2011-12-16 20:15:17 +0100

    Separate db access into a model module, use a layout template.

--- web/index.py
+++ web/index.py
@@ -21,119 +21,118 @@ from time import time
 import config
 import web
 from web import form
+import model
 
 urls = (
 '/', 'index',
-'/top', 'top',
 '/about', 'about',
 '/([a-z]*)/([0-9]{6})/(.*)', 'result',
+'/([a-z]*)/latest/(.*)', 'latest_result',
+'/([a-z]*)/top', 'latest_top',
+
 )
 
 app = web.application(urls, globals())
 
-LATESTTOP = "201012"
 
-PROJECTS = [("English","en"),
-            ("German","de"),
-            ("French","fr"),
-            ("Polish","pl"),
-            ("Japanese","ja"),
-            ("Italian","it"),
-            ("Dutch","nl"),
-            ("Portuguese","pt"),
-            ("Spanish","es"),
-            ("Swedish","sv"),
-            ("Russian","ru"),
-            ("Chinese","zh"),
-            ("Chinese (classical)","zh-classical"),
-            ("Norwegian","no"),
-            ("Finnish","fi"),
-            ("Volapük","vo"),
-            ("Catalan","ca"),
-            ("Romanian","ro"),
-            ("Turkish","tr"),
-            ("Ukrainian","uk"),
-            ("Esperanto","eo"),
-            ("Czech","cs"),
-            ("Hungarian","hu"),
-            ("Slovak","sk"),
-            ("Danish","da"),
-            ("Indonesian","id"),
-            ("Hebrew","he"),
-            ("Lithuanian","lt"),
-            ("Serbian","sr"),
-            ("Slovenian","sl"),
-            ("Arabic","ar"),
-            ("Korean","ko"),
-            ("Bulgarian","bg"),
-            ("Estonian","et"),
-            ("Newar / Nepal Bhasa","new"),
-            ("Croatian","hr"),
-            ("Telugu","te"),
-            ("Cebuano","ceb"),
-            ("Galician","gl"),
-            ("Thai","th"),
-            ("Greek","el"),
-            ("Persian","fa"),
-            ("Vietnamese","vi"),
-            ("Norwegian (Nynorsk)","nn"),
-            ("Malay","ms"),
-            ("Simple English","simple"),
-            ("Basque","eu"),
-            ("Bishnupriya Manipuri","bpy"),
-            ("Bosnian","bs"),
-            ("Luxembourgish","lb"),
-            ("Georgian","ka"),
-            ("Icelandic","is"),
-            ("Albanian","sq"),
-            ("Breton","br"),
-            ("Latin","la"),
-            ("Azeri","az"),
-            ("Bengali","bn"),
-            ("Hindi","hi"),
-            ("Marathi","mr"),
-            ("Tagalog","tl"),
-            ("Macedonian","mk"),
-            ("Serbo-Croatian","sh"),
-            ("Ido","io"),
-            ("Welsh","cy"),
-            ("Piedmontese","pms"),
-            ("Sundanese","su"),
-            ("Latvian","lv"),
-            ("Tamil","ta"),
-            ("Neapolitan","nap"),
-            ("Javanese","jv"),
-            ("Haitian","ht"),
-            ("Low Saxon","nds"),
-            ("Sicilian","scn"),
-            ("Occitan","oc"),
-            ("Asturian","ast"),
-            ("Kurdish","ku"),
-            ("Armenian", "hy"),
-            ("Commons","commons.m"),
-            ("Meta","meta.m")]
-
-PROJECTS = [(key, value) for value, key in PROJECTS]
+PROJECTS = [("en","English"),
+            ("de","German"),
+            ("fr","French"),
+            ("pl","Polish"),
+            ("ja","Japanese"),
+            ("it","Italian"),
+            ("nl","Dutch"),
+            ("pt","Portuguese"),
+            ("es","Spanish"),
+            ("sv","Swedish"),
+            ("ru","Russian"),
+            ("zh","Chinese"),
+            ("zh-classical","Chinese (classical)"),
+            ("no","Norwegian"),
+            ("fi","Finnish"),
+            ("vo","Volapük"),
+            ("ca","Catalan"),
+            ("ro","Romanian"),
+            ("tr","Turkish"),
+            ("uk","Ukrainian"),
+            ("eo","Esperanto"),
+            ("cs","Czech"),
+            ("hu","Hungarian"),
+            ("sk","Slovak"),
+            ("da","Danish"),
+            ("id","Indonesian"),
+            ("he","Hebrew"),
+            ("lt","Lithuanian"),
+            ("sr","Serbian"),
+            ("sl","Slovenian"),
+            ("ar","Arabic"),
+            ("ko","Korean"),
+            ("bg","Bulgarian"),
+            ("et","Estonian"),
+            ("new","Newar / Nepal Bhasa"),
+            ("hr","Croatian"),
+            ("te","Telugu"),
+            ("ceb","Cebuano"),
+            ("gl","Galician"),
+            ("th","Thai"),
+            ("el","Greek"),
+            ("fa","Persian"),
+            ("vi","Vietnamese"),
+            ("nn","Norwegian (Nynorsk)"),
+            ("ms","Malay"),
+            ("simple","Simple English"),
+            ("eu","Basque"),
+            ("bpy","Bishnupriya Manipuri"),
+            ("bs","Bosnian"),
+            ("lb","Luxembourgish"),
+            ("ka","Georgian"),
+            ("is","Icelandic"),
+            ("sq","Albanian"),
+            ("br","Breton"),
+            ("la","Latin"),
+            ("az","Azeri"),
+            ("bn","Bengali"),
+            ("hi","Hindi"),
+            ("mr","Marathi"),
+            ("tl","Tagalog"),
+            ("mk","Macedonian"),
+            ("sh","Serbo-Croatian"),
+            ("io","Ido"),
+            ("cy","Welsh"),
+            ("pms","Piedmontese"),
+            ("su","Sundanese"),
+            ("lv","Latvian"),
+            ("ta","Tamil"),
+            ("nap","Neapolitan"),
+            ("jv","Javanese"),
+            ("ht","Haitian"),
+            ("Low nds","Saxon"),
+            ("scn","Sicilian"),
+            ("oc","Occitan"),
+            ("ast","Asturian"),
+            ("ku","Kurdish"),
+            ("hy", "Armenian"),
+            ("commons.m","Commons"),
+            ("meta.m","Meta")]
 
 class base(object):
     def __init__(self, *args, **kwargs):
         super(base, self).__init__(*args, **kwargs)
-        self.db = web.database(dbn='mysql', host= config.db_host, user= config.db_user , pw=config.db_password, db='wikistats')
+        
 class about:
     def GET(self):
         render = web.template.render('templates/')
         return render.about()
 
-class top(base):
-    def GET(self):
-        proj = 'en' #Add code to parse out proj from URL.
-        rows = self.db.query("SELECT rank,project,page,hitcount FROM top_%s WHERE project='%s' ORDER BY RANK LIMIT 1000" % (LATESTTOP, proj))
-        render = web.template.render('templates/')
-        return render.top(rows, LATESTTOP)
+class latest_top(base):
+    def GET(self, proj=None):
+        rows = model.get_top(proj)
+        render = web.template.render('templates/', base = 'layout', globals = { 'unquote' : urllib.unquote })
+        return render.top(rows, config.LATESTTOP)
 
 class index(base):
     def init_form(self, prevform=None):
-        years, latest = getdates()
+        years, latest = model.get_dates()
         if not prevform:
             search = form.Form(
             form.Dropdown('proj', PROJECTS, value=latest, description=''),
@@ -150,19 +149,18 @@ class index(base):
     
     def GET(self):
         form =  self.init_form()
-        render = web.template.render('templates/', base='index')
+        render = web.template.render('templates/', base='layout')
         return render.form(form)
         
     def POST(self): 
         search = web.input()       
         return web.redirect('/%s/%s/%s' % (search['proj'], search['year'], search['inputbox']))
     
-  
-
+    
 class result(base): 
     def GET(self,proj=None, year=None, page=None):
-        render = web.template.render('templates/', base='index')
-        search =dict(proj=proj, year=year, inputbox=page)
+        render = web.template.render('templates/', base='layout')
+        search = dict(proj=proj, year=year, inputbox=page)
         idx =index()
         form =  idx.init_form(search)
 
@@ -206,137 +204,30 @@ class result(base):
         return total_hits_html
     
     def fetch_results(self, search):
-        start = time()
         today = datetime.date.today()
         page = search['inputbox']
         date = search['year']
         proj = search['proj']
-    
-        days = range(1,32)
         page = urllib.unquote(page).strip().replace(" ","_")
-        
-        if date == -1:
-            startd=today-datetime.timedelta(days=30)
-            ix = startd
-            for i in range(0,31):
-                days[i] = ix.day
-                ix += datetime.timedelta(days=1)
-            months = getdates((startd.year, startd.month))[0]
-            counts = []
-            for m in months:
-                try:
-                    counts.extend(getcounts_new(self.db,m,proj,page).values())
-                except:
-                    counts.extend(getcounts(self.db,m,proj,page).values())
-            counts = counts[-30:]
-    
+
+        rank = model.get_rank(page, proj)
+
+        if date == 'latest':
+            page_counts, execution_time = model.get_latest_stats(page,proj)
         else:
-            try:
-                counts = getcounts_new(self.db,date,proj,page).values()
-            except:
-                counts = getcounts(self.db,date,proj,page).values()
+            page_counts, execution_time = model.get_monthly_stats(page,date,proj)
+        
+
+        max_count = max(float(max(page_counts)), 1)
+
+        return page_counts, max_count, rank, date, execution_time
+
+# XXX: Hack to support latest 30 days view, rewrite to be prettier.
+class latest_result(result):
+    def GET(self,proj=None, page=None):
+        return result.GET(self, proj, 'latest', page)
+
     
-        try:
-            maxcount = float(max(counts))
-        except:
-            maxcount = 1
-    
-        if maxcount == 0:
-            maxcount = 1
-    
-        rank = self.db.query("SELECT rank FROM top_%s WHERE project='%s' AND page='%s'" % (LATESTTOP,proj,page))
-        #print 'RANK: %s' % dir(rank), rank.c, rank.i
-#        try:
-#            rank = c.fetchone()[0]
-#        except:
-#            rank = -1
-            
-        end = time()
-        dt = end-start
-        return counts, maxcount, rank, date, dt
-
-
-def getdates(start = None):
-    if start == None:
-        start = (2007, 12)
-    today = datetime.date.today()
-    end = (today.year, today.month)
-
-    x = start
-    ret = []
-    while x <= end:
-        year, month = x
-        ret.append("%d%02d" % (year, month))
-        month = month + 1
-        if month > 12:
-            month = 1
-            year = year + 1
-        x = (year, month)
-
-    latest = "%d%02d" % end
-    return ret, latest
-
-def getalldays(c,date):
-    rows = c.query("SHOW TABLES like 'pagecounts_%s__';" % date)
-    #c.execute("SHOW TABLES like 'pagecounts_%s__';" % date)
-    #rows = c.fetchall()
-    all_days = [x.values()[0] for x in rows]
-    return all_days
-
-def getalldays_new(c,date):
-    #c.execute("SHOW TABLES like 'pagecounts_%s';" % date)
-    rows =c.query("SHOW TABLES like 'pagecounts_%s';" % date)
-    #rows = c.fetchall()
-    all_days = [x.values()[0] for x in rows]
-    return all_days
-
-
-def getcounts(c,date,proj,page):
-    all_days = getalldays(c,date)
-    counts = {}
-    for day in all_days:
-        count = c.query("SELECT sum(hitcount) FROM "+day+" WHERE page=$page AND project=$project;", vars = {'page' : page, 'project' : proj })
-        #c.execute("SELECT sum(hitcount) FROM "+day+" WHERE page=%s AND project=%s;", (page,proj))
-        #count = c.fetchone()[0]
-        day = int(day.split("_")[1][-2:])
-        #print dir(count)
-        try:
-            counts[day] = int(count[0].values()[0])
-        except TypeError:
-            counts[day] = 0
-
-    return counts
-
-def getcounts_new(c,date,proj,page):
-
-    tables = getalldays_new(c,date)
-    counts = {}
-    for i in range(0,32):
-        counts[i]=0
-
-    if len(tables) != 1:
-        raise "Not in new format"
-
-    table = tables[0]
-
-    #c.execute("SELECT sum(hitcount),sum(d1), sum(d2), sum(d3), sum(d4), sum(d5), sum(d6), sum(d7), sum(d8), sum(d9), sum(d10), sum(d11), sum(d12), sum(d13), sum(d14), sum(d15), sum(d16), sum(d17), sum(d18), sum(d19), sum(d20), sum(d21), sum(d22), sum(d23), sum(d24), sum(d25), sum(d26), sum(d27), sum(d28), sum(d29), sum(d30), sum(d31) FROM "+table+" WHERE page=%s AND project=%s;", (page,proj))
-    count = c.query("SELECT sum(hitcount),sum(d1), sum(d2), sum(d3), sum(d4), sum(d5), sum(d6), sum(d7), sum(d8), sum(d9), sum(d10), sum(d11), sum(d12), sum(d13), sum(d14), sum(d15), sum(d16), sum(d17), sum(d18), sum(d19), sum(d20), sum(d21), sum(d22), sum(d23), sum(d24), sum(d25), sum(d26), sum(d27), sum(d28), sum(d29), sum(d30), sum(d31) FROM "+table+" WHERE page=%s AND project=%s;", (page,proj))
-    #count = c.fetchone()
-
-    # One time bug in the database generation script
-    # caused this months dates to be offset for one
-    # day.
-    if date == "200806":
-        i = 0
-    else:
-        i = 1
-
-    for x in count[1:]:
-        counts[i]=int(x)
-        i=i+1
-
-    return counts
-
 if __name__ == '__main__':
     if config.DEBUG:
         app.run()
---
+++ web/model.py
@@ -0,0 +1,130 @@
+import datetime, time
+import web
+import config
+
+
+db = web.database(dbn='mysql', host=config.db_host, user= config.db_user , pw=config.db_password, db='wikistats')
+
+
+def get_dates(start = None):
+    if start == None:
+        start = (2007, 12)
+    today = datetime.date.today()
+    end = (today.year, today.month)
+
+    x = start
+    ret = []
+    while x <= end:
+        year, month = x
+        ret.append("%d%02d" % (year, month))
+        month = month + 1
+        if month > 12:
+            month = 1
+            year = year + 1
+        x = (year, month)
+
+    latest = "%d%02d" % end
+    return ret, latest
+
+
+def get_top(proj):
+    return db.query("SELECT rank,project,page,hitcount FROM top_%s WHERE project='%s' ORDER BY RANK LIMIT 1000" % (config.LATESTTOP, proj))
+            
+def get_rank(page,  proj):
+     rank = db.query("SELECT rank FROM top_%s WHERE project='%s' AND page='%s'" % (config.LATESTTOP,proj,page))
+
+     return rank
+
+def get_latest_stats(page, proj):
+    ''' Fetch statistics in a list of view counts, for the latest 30 days '''
+    start = time.time()
+    
+    days = range(1,32)
+    today = datetime.date.today()
+        
+    startd=today-datetime.timedelta(days=30)
+    ix = startd
+    for i in range(0,31):
+        days[i] = ix.day
+        ix += datetime.timedelta(days=1)
+    months = get_dates((startd.year, startd.month))[0]
+    page_counts = []
+    for m in months:
+        try:
+            page_counts.extend(_getcounts_new(db,m,proj,page).values())
+        except:
+            page_counts.extend(_getcounts(db,m,proj,page).values())
+    page_counts = page_counts[-30:]
+ 
+    end = time.time()
+    execution_time = end-start
+    return page_counts, execution_time
+
+def get_monthly_stats(page, date, proj):
+    ''' Fetch statistics as a list of view counts, for a given month'''
+    start = time.time()
+
+    try:
+        page_counts = _getcounts_new(db,date,proj,page).values()
+    except:
+        page_counts = _getcounts(db,date,proj,page).values()   
+
+    end = time.time()
+    execution_time = end-start
+    return page_counts, execution_time
+
+
+
+def _getalldays(c,date):
+    rows = c.query("SHOW TABLES like 'pagecounts_%s__';" % date)
+    all_days = [x.values()[0] for x in rows]
+    return all_days
+
+def _getalldays_new(c,date):
+    rows =c.query("SHOW TABLES like 'pagecounts_%s';" % date)
+    all_days = [x.values()[0] for x in rows]
+    return all_days
+
+
+def _getcounts(c,date,proj,page):
+    all_days = _getalldays(c,date)
+    counts = {}
+    for day in all_days:
+        count = c.query("SELECT sum(hitcount) FROM "+day+" WHERE page=$page AND project=$project;", vars = {'page' : page, 'project' : proj })
+        day = int(day.split("_")[1][-2:])
+        try:
+            counts[day] = int(count[0].values()[0])
+        except TypeError:
+            counts[day] = 0
+
+    return counts
+
+def _getcounts_new(c,date,proj,page):
+
+    tables = _getalldays_new(c,date)
+    counts = {}
+    for i in range(0,32):
+        counts[i]=0
+
+    if len(tables) != 1:
+        raise "Not in new format"
+
+    table = tables[0]
+
+
+    count = c.query("SELECT sum(hitcount),sum(d1), sum(d2), sum(d3), sum(d4), sum(d5), sum(d6), sum(d7), sum(d8), sum(d9), sum(d10), sum(d11), sum(d12), sum(d13), sum(d14), sum(d15), sum(d16), sum(d17), sum(d18), sum(d19), sum(d20), sum(d21), sum(d22), sum(d23), sum(d24), sum(d25), sum(d26), sum(d27), sum(d28), sum(d29), sum(d30), sum(d31) FROM "+table+" WHERE page=$page AND project=$project;", vars = {'page' : page, 'project' : proj })
+
+    # One time bug in the database generation script
+    # caused this months dates to be offset for one
+    # day.
+    if date == "200806":
+        i = 0
+    else:
+        i = 1
+
+    for x in count[1:]:
+        counts[i]=int(count[0].values()[0])
+        i=i+1
+
+    return counts
+
